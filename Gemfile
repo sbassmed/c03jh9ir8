b890062dec08b1dca23ced6690fe08c2e8c9728a
sbassmed <sebastiancamilomarin@gmail.com>
2019-01-28 13:21:08 -0500

    creando rutas y vistas

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '5.0.2'
+gem 'bootstrap-sass', '~> 3.4.0'
+#gem 'jquery-rails', '~> 4.3', '>= 4.3.3'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
   # Use SCSS for stylesheets      # Use SCSS for stylesheets
--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -2,4 +2,23 @@ class ActorsController < ApplicationController
   def index
     @actors = Actor.all
   end
+  def new
+    @actor = Actor.new
+  end
+
+  def create
+    @actor = Actor.new(actor_params)
+    if @actor.save
+      redirect_to actors_path, notice: "El Actor fue publicado con éxito"
+    else
+      render :new
+    end
+  end
+
+
+ private
+ def actor_params
+   params.require(:actor).permit(:name, :bio, :image_url)
+ end
+
 end
